75a8298123b52dd86629c9b092da99991bf0c666
6417 Fridolins Robotik <fridolins@users.noreply.github.com>
2018-01-18 22:34:12 +0100

    Initial commit

---
+++ src/org/usfirst/frc/team6417/robot/Robot.java
@@ -0,0 +1,101 @@
+
+package org.usfirst.frc.team6417.robot;
+
+
+
+import org.usfirst.frc.team6417.robot.subsystems.NavX;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.command.Scheduler;
+
+public class Robot extends IterativeRobot {
+
+	// Controllers
+	public static Joystick joystickOne;
+	public static Joystick joystickTwo;
+	public static JoystickButton shootJoystick, shootXBox, collectJoystick, collectXBox, reverseJoystick, reverseXBox;
+	public static OI oi;
+
+	// Subsystems
+	public static NavX navX;
+
+	// Commands
+
+
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
+		joystickOne = new Joystick(RobotMap.CONTROLLER.RIGHT);
+		joystickTwo = new Joystick(RobotMap.CONTROLLER.LEFT);
+
+		navX = new NavX();
+		oi = new OI();
+	}
+
+	/**
+	 * 
+	 * This function is called once each time the robot enters Disabled mode.
+	 * You can use it to reset any subsystem information you want to clear when
+	 * the robot is disabled.
+	 */
+	@Override
+	public void disabledInit() {
+	}
+
+	@Override
+	public void disabledPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select
+	 * between different autonomous modes using the dashboard. The sendable
+	 * chooser code works with the Java SmartDashboard. If you prefer the
+	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+	 * getString code to get the auto name from the text box below the Gyro
+	 *
+	 * You can add additional auto modes by adding additional commands to the
+	 * chooser code above (like the commented example) or additional comparisons
+	 * to the switch structure below with additional strings & commands.
+	 */
+	@Override
+	public void autonomousInit() {
+	}
+
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	@Override
+	public void autonomousPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	@Override
+	public void teleopInit() {
+	}
+
+	/**
+	 * This function is called periodically during operator control
+	 */
+	@Override
+	public void teleopPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	@Override
+	public void robotPeriodic() {
+		
+	}
+
+	/**
+	 * This function is called periodically during test mode
+	 */
+	@Override
+	public void testPeriodic() {
+	}
+}
---
+++ src/org/usfirst/frc/team6417/robot/RobotMap.java
@@ -0,0 +1,109 @@
+package org.usfirst.frc.team6417.robot;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+
+	/**
+	 * The RobotMap.MATH class provides you with different mathematical values
+	 */
+	public static class MATH {
+		public static final double PI = 3.14159265359;
+	}
+	
+	public static class SPEED {
+	}
+
+	/**
+	 * In the RobotMap.MOTOR class the motor ports are
+	 */
+	public static class MOTOR {
+	}
+
+	/**
+	 * In the RobotMap.ENCODER class you'll find different the ports of the
+	 * encoder and values to the encoder
+	 */
+	public static class ENCODER {
+		public static final int PULSE_PER_ROTATION = 512;
+	}
+
+	/**
+	 * In the RobotMap.PNEUMATICS class you'll find different port definitions
+	 * so you can use your pneumatics
+	 */
+	public static class PNEUMATICS {
+		public static final int COMPRESSOR_PORT = 0;
+		public static final int EXTEND_PORT = 0;
+		public static final int RETRACT_PORT = 1;
+	}
+
+	public static class CONTROLLER {
+		public static final int RIGHT = 0;
+		public static final int LEFT = 1;
+	}
+
+	/**
+	 * In the RobotMap.JOYSTICK class you'll find the ports of the joysticks
+	 */
+	public static class JOYSTICK {
+
+		public static class AXIS {
+			public static final int X = 0;
+			public static final int Y = 1;
+			public static final int TWIST = 2;
+			public static final int SLIDER = 3;
+		}
+
+		public static class BUTTONS {
+		}
+
+		/**
+		 * contains Deadzones for different Joysticks
+		 */
+		public static class DEADZONES {
+			public static final double JOYSTICK1_X = 0.05;
+			public static final double JOYSTICK1_Y = 0.05;
+			public static final double JOYSTICK1_TWIST = 0.08;
+
+			public static final double JOYSTICK2_X = 0.05;
+			public static final double JOYSTICK2_Y = 0.05;
+			public static final double JOYSTICK2_TWIST = 0.015;
+		}
+	}
+
+	public static class XBOX {
+
+		public static class AXIS {
+			public static final int LEFT_X = 0;
+			public static final int LEFT_Y = 1;
+			public static final int RIGHT_X = 4;
+			public static final int RIGHT_Y = 5;
+		}
+
+		public static class BUTTONS {
+		}
+
+		public static class DEADZONES {
+			public static final double XBOX_LEFT_X = 0;
+			public static final double XBOX_LEFT_Y = 0;
+			public static final double XBOX_RIGHT_X = 0;
+			public static final double XBOX_RIGHT_Y = 0;
+		}
+	}
+
+	/**
+	 * In the RobotMap.ROBOT class you'll find different values concerning your
+	 * robot
+	 */
+	public static class ROBOT {
+		public static final double WHEEL_DIAMETER = 15.24; /*cm*/
+		public static final double WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER * MATH.PI; /*cm*/
+		public static final double DIAGONAL_DISTANCE_BETWEEN_WHEELS = 0.0; /*cm*/
+		public static final double DIST_PER_PULSE = ROBOT.WHEEL_CIRCUMFERENCE / ENCODER.PULSE_PER_ROTATION; /*cm*/
+	}
+}
