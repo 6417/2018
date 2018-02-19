7ad97df70580c2687a741271a95cfbd29f470926
6417 Fridolins Robotik <34323339+Fridolins@users.noreply.github.com>
2018-02-19 21:07:37 +0100

    Extended the SwerveDriveWheelCheck to also check the 0-point sensor.

--- src/org/usfirst/frc/team6417/robot/commands/SwerveDriveWheelCheck.java
+++ src/org/usfirst/frc/team6417/robot/commands/SwerveDriveWheelCheck.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team6417.robot.commands;
 
 import org.usfirst.frc.team6417.robot.MotorController;
 import org.usfirst.frc.team6417.robot.Robot;
+import org.usfirst.frc.team6417.robot.subsystems.SwerveWheelDrive;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
@@ -15,18 +16,42 @@ public final class SwerveDriveWheelCheck extends Command {
 	
 	@Override
 	protected void execute() {
-		checkMotors(Robot.swerveDrive.frontLeft.angleMotor, Robot.swerveDrive.frontLeft.velocityMotor);
-		checkMotors(Robot.swerveDrive.frontRight.angleMotor, Robot.swerveDrive.frontRight.velocityMotor);
-		checkMotors(Robot.swerveDrive.backLeft.angleMotor, Robot.swerveDrive.backLeft.velocityMotor);
-		checkMotors(Robot.swerveDrive.backRight.angleMotor, Robot.swerveDrive.backRight.velocityMotor);
+		System.out.println("SwerveDriveWheelCheck. This is goint to take while ...");
+		
+		checkMotors("FL", Robot.swerveDrive.frontLeft.angleMotor, Robot.swerveDrive.frontLeft.velocityMotor);
+		checkMotors("FR", Robot.swerveDrive.frontRight.angleMotor, Robot.swerveDrive.frontRight.velocityMotor);
+		checkMotors("BL", Robot.swerveDrive.backLeft.angleMotor, Robot.swerveDrive.backLeft.velocityMotor);
+		checkMotors("BR", Robot.swerveDrive.backRight.angleMotor, Robot.swerveDrive.backRight.velocityMotor);
+		
+		checkZeroPoint("FL", Robot.swerveDrive.frontLeft);
+		checkZeroPoint("FR", Robot.swerveDrive.frontRight);
+		checkZeroPoint("BL", Robot.swerveDrive.backLeft);
+		checkZeroPoint("BR", Robot.swerveDrive.backRight);
+
+		System.out.println("SwerveDriveWheelCheck. Done.");
+	}
+	
+	private void checkZeroPoint(String label, SwerveWheelDrive swerveWheelDrive) {
+		System.out.print("Checking "+label+" position sensor ...");
+		swerveWheelDrive.startZeroPointCalibration();
+		while(!swerveWheelDrive.isOnZeroPoint()) {
+			System.out.print(".");
+			swerveWheelDrive.tick();
+			Timer.delay(0.002);
+		}
+		System.out.println();
+		System.out.print("Checking "+label+" position sensor DONE");
 	}
 
-	private void checkMotors(MotorController angleMotor, MotorController velocityMotor) {
+	private void checkMotors(String label, MotorController angleMotor, MotorController velocityMotor) {
+		System.out.println("Checking "+label);
+		System.out.println("Checking angle motor "+label);
 		checkMotor(angleMotor);
+		System.out.println("Checking velocity motor "+label);
 		checkMotor(velocityMotor);
 	}
 
-	private void checkMotor(MotorController motor) {
+	private void checkMotor(MotorController motor) {		
 		motor.set(0.3);
 		Timer.delay(5);
 		motor.set(0.0);
