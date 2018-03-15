1b3ba54af838c0815780b4778582052f099aa269
6417 Fridolins Robotik <34323339+Fridolins@users.noreply.github.com>
2018-03-15 18:02:12 +0100

    Calibration-behavior now respects that subsystems can be on and off in the source-code.

--- src/org/usfirst/frc/team6417/robot/commands/CalibrationBehavior.java
+++ src/org/usfirst/frc/team6417/robot/commands/CalibrationBehavior.java
@@ -6,11 +6,17 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public final class CalibrationBehavior extends CommandGroup {
 	public CalibrationBehavior() {
-		addSequential(new LiftingUnitFindEndpointDown());
-		addSequential(new LiftingUnitMoveToPosition(RobotMap.ROBOT.LIFTING_UNIT_SWITCH_ALTITUDE_IN_TICKS));
-		addSequential(new LiftingUnitWagonFindEndpointFront());
-		addSequential(new SwerveDriveWheelsToZeroPosition());
-		addSequential(new SwerveDriveWheelsToParallelPosition());
-		addSequential(new SwerveDriveSetPosToZero());
+		if(RobotMap.SUBSYSTEM.IS_LIFTING_UNIT_IN_USE) {
+			addSequential(new LiftingUnitFindEndpointDown());
+			addSequential(new LiftingUnitMoveToPosition(RobotMap.ROBOT.LIFTING_UNIT_SWITCH_ALTITUDE_IN_TICKS));
+		}
+		if(RobotMap.SUBSYSTEM.IS_LIFTING_UNIT_WAGON_IN_USE) {
+			addSequential(new LiftingUnitWagonFindEndpointFront());
+		}
+		if(RobotMap.SUBSYSTEM.IS_SWERVE_DRIVE_IN_USE) {
+			addSequential(new SwerveDriveWheelsToZeroPosition());
+			addSequential(new SwerveDriveWheelsToParallelPosition());
+			addSequential(new SwerveDriveSetPosToZero());
+		}
 	}
 }
