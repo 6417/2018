4c56d5bf636530998a1b64838ebac7c0a1f02200
6417 Fridolins Robotik <34323339+Fridolins@users.noreply.github.com>
2018-03-19 23:11:36 +0100

    Make shure no lifting-unit up movement when in position full-back

--- src/org/usfirst/frc/team6417/robot/commands/PrepareRobotElevationBehavior.java
+++ src/org/usfirst/frc/team6417/robot/commands/PrepareRobotElevationBehavior.java
@@ -1,13 +1,15 @@
 package org.usfirst.frc.team6417.robot.commands;
 
 import org.usfirst.frc.team6417.robot.RobotMap;
+import org.usfirst.frc.team6417.robot.subsystems.LiftingUnitWagon;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public final class PrepareRobotElevationBehavior extends CommandGroup {
 	public PrepareRobotElevationBehavior() {
 		addSequential(new LiftingUnitMoveToSavePosition());
-		addSequential(new LiftingUnitWagonMoveToFullBack());
+		addSequential(new LiftingUnitWagonMove(LiftingUnitWagon.BACK));
 		addParallel(new LiftingUnitMoveToPosition(RobotMap.ROBOT.LIFTING_UNIT_HANGING_ALTITUDE_IN_TICKS));
+		addSequential(new LiftingUnitWagonMoveToFullBack());
 	}
 }
