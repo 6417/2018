54019788c86c4c8d88e34efa4f2058f4fe504f85
6417 Fridolins Robotik <34323339+Fridolins@users.noreply.github.com>
2018-03-19 21:37:19 +0100

    Switched the joystick inputs to use the y axis to move the lifting-unit-wagon.

--- src/org/usfirst/frc/team6417/robot/commands/LiftingUnitWagonTeleoperated.java
+++ src/org/usfirst/frc/team6417/robot/commands/LiftingUnitWagonTeleoperated.java
@@ -14,15 +14,15 @@ public final class LiftingUnitWagonTeleoperated extends Command {
 
 	@Override
 	protected void execute() {
-		double x = OI.getInstance().liftingUnitController.getRawAxis(4);
-		if(Math.abs(x) <= RobotMap.JOYSTICK.DEADZONES.JOYSTICK2_X) {
-			x = 0.0;
+		double y = OI.getInstance().liftingUnitController.getRawAxis(5);
+		if(Math.abs(y) <= RobotMap.JOYSTICK.DEADZONES.JOYSTICK2_X) {
+			y = 0.0;
 		}
 		
-		if(x == 0.0) {
+		if(y == 0.0) {
 			Robot.liftingUnitWagon.holdPosition();
 		} else {
-			Robot.liftingUnitWagon.move(x);
+			Robot.liftingUnitWagon.move(-y);
 		}
 	}
 	
